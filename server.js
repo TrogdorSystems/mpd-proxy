26721bda350c32c0e9907cd9788af80bacd67fac
tmkuba <tmk.rss@gmail.com>
2018-03-06 10:12:35 -0800

    Rendering 4 components client-side

---
+++ server.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const _ = require('underscore');
+
+const app = express();
+
+app.use(express.static('./dist'));
+
+// load the template file
+const compiled = _.template(fs.readFileSync('./dist/template.html', 'utf-8'));
+
+app.get('/:id', (req, res) => {
+  // inject id into the template
+  const output = compiled({id: req.params.id});
+
+  // serve back to the client
+  res.send(output);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log('app is listening on', port);
+});
