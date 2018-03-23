88f7e8186797deed7bc94f865ecd34d7c2718499
Michael Durfey <mdurfey@me.com>
2018-03-23 00:38:37 -0700

    root route setup and rendering

---
+++ dist/html.js
@@ -0,0 +1,17 @@
+const Html = ( component, title, id ) => `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>${title}</title>
+      <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+      <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+      <script type="text/javascript" src="/productionBundle.js"></script>
+      </head>
+      <body>
+      <div id="app">${component}</div>
+      <script>ReactDOM.hydrate(React.createElement(Reservation, { id: ${id} }), document.getElementById('app'))</script>
+    </body>
+  </html>
+`;
+
+module.exports = Html;
--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const fs = require('fs');
-const express = require('express');
 const _ = require('underscore');
+const Html = require('./dist/html.js')
+const http = require('http');
+const request = require('request');
+const { renderToString } = require('react-dom/server');
+const { createElement } = require('react');
+const reservationsBundle = require('./dist/productionBundle-server').default;
 
-const app = express();
+const port = 8000;
+const hostname = '127.0.0.1';
 
-app.use(express.static('./dist'));
+const server = http.createServer((req, res) => {
+  let rendercomponent = (component, id) => {
+    let componentString = createElement(component, { id });
+    return renderToString(componentString);
+  };
+  const { method, url } = req;
+  const urlSplit = url.split('/');
+  let id = urlSplit[2] !== 'undefined' ? urlSplit[2] : 305;
+  if (method === 'GET' && url === `/restaurants/${urlSplit[2]}/reservations/${urlSplit[4]}`) {
+      request(`http://localhost:8081${url}`).pipe(res);
+  } else if (method === 'GET' && url === `/productionBundle.js`){
+    // res.writeHead(200, { 'Content-Type': 'text/html'})
+    request(`http://localhost:8081/productionBundle.js`).pipe(res);
+  } else if (method === 'GET' && url === '/') {
+    res.end(Html(rendercomponent(reservationsBundle, 305), 'silverspoon', 305));
+  } else if (method === 'GET' && url === '/favicon.ico') {
+    res.writeHead(304)
+    res.end()
+  }
 
-// load the template file
-const compiled = _.template(fs.readFileSync('./dist/template.html', 'utf-8'));
-
-app.get('/:id', (req, res) => {
-  // inject id into the template
-  const output = compiled({id: req.params.id});
-
-  // serve back to the client
-  res.send(output);
-});
-
-const port = process.env.PORT || 8081;
-
-app.listen(port, () => {
-  console.log('app is listening on', port);
-});
+})
+// app.use(express.static('./dist'));
+server.listen(port, hostname, () => console.log(`server listening on ${port}`));
+// // load the template file
+// const compiled = _.template(fs.readFileSync('./dist/template.html', 'utf-8'))
